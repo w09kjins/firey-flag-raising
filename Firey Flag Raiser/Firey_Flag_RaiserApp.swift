d6bb8ad6b6290ebf7d9c4e262fbb72ad99f0b160
Trinity <trinitylee08@rafflesgirlssch.edu.sg>
2021-09-06 10:13:50 +0800

    Initial Commit

---
+++ Firey Flag Raiser/Firey_Flag_RaiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firey_Flag_RaiserApp.swift
+//  Firey Flag Raiser
+//
+//  Created by Trinity Lee on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Firey_Flag_RaiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
